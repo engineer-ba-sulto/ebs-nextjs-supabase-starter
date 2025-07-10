aa7de3504a9469cac4e0f9f65a9049abe7e5c2af
Hiroki Chiyoda <contact@arafipro.com>
2025-07-10 16:28:12 +0900

    LPページとサービスページを変更

--- app/(LP)/page.tsx
+++ app/(LP)/page.tsx
@@ -2,12 +2,11 @@ import { Hero } from "./_components/hero";
 
 export default function Home() {
   return (
-    <div className="flex-1 flex flex-col gap-20 p-5">
+    <main className="flex-1 flex flex-col gap-20 p-5">
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 max-w-5xl px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        <p>アプリケーションの構築を始めましょう！</p>
-      </main>
-    </div>
+      <div className="flex-1 flex flex-col gap-6 max-w-5xl px-4">
+        <h2 className="font-bold text-2xl mb-4">LP Page</h2>
+      </div>
+    </main>
   );
 }
