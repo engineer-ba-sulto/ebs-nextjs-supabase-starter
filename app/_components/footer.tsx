7b4c7eceb9a6547b7a0bdddd4432541f5709ec8f
Hiroki Chiyoda <contact@arafipro.com>
2025-07-10 16:59:59 +0900

    フッターコンポーネントのスタイルを修正

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+    <footer className="w-full flex items-center justify-center border-t text-center py-16">
       Footer
     </footer>
   );
